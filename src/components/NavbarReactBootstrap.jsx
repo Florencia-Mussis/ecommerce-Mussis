aa9e19d517ccc91fa771aff0044782d67b6470c6
Florencia-Mussis <mussisflorencia@gmail.com>
2024-11-01 15:12:34 +0100

    Create CNavbarReactBootstrap component

---
+++ src/components/NavbarReactBootstrap.jsx
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import CartWidget from './CartWidget';
+import Culture_logo from '../assets/Culture_logo.png';
+
+function NavbarReactBootstrap() {
+  return (
+    <Navbar expand="lg" className="navbar-font">
+      <Container>
+        <Navbar.Brand href="#home">
+            <img src={Culture_logo} alt='logo'/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto justify-content-center w-100 gap-5">
+            <Nav.Link href="#home" className="text-uppercase fs-6 text-dark">Mujer</Nav.Link>
+            <Nav.Link href="#link" className="text-uppercase fs-6 text-dark">Hombre</Nav.Link>
+            <Nav.Link href="#link" className="text-uppercase fs-6 text-dark">Niña</Nav.Link>
+            <Nav.Link href="#link" className="text-uppercase fs-6 text-dark">Niño</Nav.Link>
+          </Nav>
+          <CartWidget/>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarReactBootstrap;
