441f8499cd55600c1e853af68a82a2dd0b5527ba
Florencia-Mussis <mussisflorencia@gmail.com>
2024-11-01 15:12:06 +0100

    Create CartWidget component

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,8 +1,11 @@
-const CartWidget = ({counter}) => {
-    return(
-        <div className="cartWidgetContainer">
-            <img src="./carrito-de-compras.png" alt='cart' className="cartIcon"/>
-            <p className="cartCounter">{counter}</p>
+import { PiBagLight } from "react-icons/pi";
+import { Badge } from "react-bootstrap";
+
+const CartWidget = () => {
+    return (
+        <div className="d-flex position-relative pt-2 pb-2">
+            <PiBagLight fontSize={'24px'}/>
+            <Badge className="p-0" bg="Light" text="dark">3</Badge>
         </div>
     )
 }
