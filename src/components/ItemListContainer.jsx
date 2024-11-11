60cc1cbb2a6a41370007aaa08bba8225748f4290
Florencia-Mussis <mussisflorencia@gmail.com>
2024-11-11 18:35:01 +0100

    create item list container component

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,7 +1,23 @@
+import React, {useState, useEffect} from "react"
+import { getProducts } from "../mock/data"
+import ItemList from "./ItemList"
+
 const ItemListContainer = ({greeting}) => {
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(()=>{
+        setLoading(true)
+        getProducts()
+        .then((res)=> setProducts(res))
+        .catch((error)=> console.log(error))
+        .finally(()=> setLoading(false))
+    },[])
+
     return (
         <div>
-            <h1 className="text-center text-uppercase fs-1 p-2">{greeting}</h1>
+            {/* <h1 classNameName="text-center text-uppercase fs-1 p-2">{greeting}</h1> */}
+            {loading ? <p>Cargando...</p> : <ItemList products={products}/>}
         </div>
     )
 }
