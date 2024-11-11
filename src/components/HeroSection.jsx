9634a9893a626370baa19b2562982f35859ea0cb
Florencia-Mussis <mussisflorencia@gmail.com>
2024-11-11 18:34:25 +0100

    create hero component

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,26 @@
-const HeroSection = ({backgroundImage, title, subtitle}) => {
-    return (
-        <div className="Hero-content" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover'}}>
-            <h1>{title}</h1>
-            <p>{subtitle}</p>
-        </div>
-    )
-}
+import Hero_homePage from '../assets/Hero_homePage.png'
+import Hero_homePage_mobile from '../assets/Hero_homePage_mobile.png'
 
-export default HeroSection
+
+const HeroSection = ({ title, subtitle }) => {
+    return (
+        <div className="hero-content text-center d-flex align-items-center justify-content-center flex-column">
+            <img
+                src={Hero_homePage}
+                alt="Hero Background"
+                className="img-fluid d-none d-md-block" /* Imagen solo para escritorio */
+            />
+            <img
+                src={Hero_homePage_mobile}
+                alt="Hero Background Mobile"
+                className="img-fluid d-md-none" /* Imagen solo para móviles */
+            />
+            <div className="text-overlay">
+                <h1>{title}</h1>
+                <p>{subtitle}</p>
+            </div>
+        </div>
+    );
+};
+
+export default HeroSection;
