b5119c43a8ae887e0c8b89bf71aeabdd79ba62d6
Florencia-Mussis <mussisflorencia@gmail.com>
2024-11-17 13:05:31 +0100

    create Error component

---
+++ src/components/Error.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Error = () => {
+    return (
+        <div>
+            <img src="" alt="error"/>
+            <Link to={'/'}>Volver a home</Link>
+        </div>
+    )
+}
+
+export default Error
