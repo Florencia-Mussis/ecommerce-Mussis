9b40fe56348b41312d854efbd57bdf0e8d118cdd
Florencia-Mussis <mussisflorencia@gmail.com>
2024-11-17 13:05:00 +0100

    update ItemList component

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,10 +3,10 @@ import Item from "./Item"
 
 const ItemList = ({products}) => {
     return (
-        <div className='item-list-container row'>
+        <div className='item-list-container row' id="all-products">
             {products.map((product)=> (
-            <div className='col-6 col-md-4 col-lg-3 mb-5'>
-                <Item product={product} key={product.id}/>
+            <div className='col-6 col-md-4 col-lg-3 mb-5' key={product.id}>
+                <Item product={product}/>
             </div>
             ))}
         </div>
