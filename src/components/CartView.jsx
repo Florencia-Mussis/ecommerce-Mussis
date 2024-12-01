08bcbd4538c09575c170434a887abb484004c0ad
Florencia-Mussis <mussisflorencia@gmail.com>
2024-12-01 12:34:52 +0100

    create CartView component

---
+++ src/components/CartView.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useCart } from '../context/CartContext';
+import EmptyCart from "./EmptyCart";
+import CartList from "./CartList";
+
+const CartView = () => {
+    const {cart} = useCart()
+
+    return (
+        <div>
+            {!cart.length ? <EmptyCart/> : <div> <CartList/> </div>}
+        </div>
+    )
+}
+
+export default CartView
+
