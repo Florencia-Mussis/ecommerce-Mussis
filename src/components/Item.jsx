403ddbc0a802a2f81071048bfff8faa14fc3ceb0
Florencia-Mussis <mussisflorencia@gmail.com>
2024-12-01 12:37:27 +0100

    add lazy loading

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,7 +5,7 @@ const Item = ({product}) => {
     return(
         <Link to={`/item/${product.id}`} className="item-link">
             <div className="card order border-0 gap-2">
-                <img src={product.img} className="card-img-top rounded-0" alt="{product.title}"/>
+                <img src={product.img} className="card-img-top rounded-0" alt="{product.title}" loading="lazy"/>
                 <div className="card-body d-flex flex-column p-0 gap-2 text-start">
                     <p className="card-tag text-uppercase m-0">New</p>
                     <h5 className="card-title m-0">{product.name}</h5>
