718f2d3bceb4d864f51920d019219aa7a89e8fae
Florencia-Mussis <mussisflorencia@gmail.com>
2024-12-06 12:18:03 +0100

    implement react-hook-form

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -5,36 +5,27 @@ import { useState } from 'react';
 import { CartContext } from '../context/CartContext';
 import { addDoc, doc, collection, getDoc, serverTimestamp, updateDoc } from 'firebase/firestore';
 import { db } from '../services/firebase';
+import { useForm } from 'react-hook-form';
 import SuccessfulOrder from './SuccessfulOrder';
 
-
 function Checkout() {
-  const [user, setUser] = useState([])
-  const [validate, setValidate] = useState('')
   const [orderId, setOrderId] = useState('')
   const {cart, totalCart, clear} = useContext(CartContext)
+  const {register, handleSubmit, formState:{errors}, getValues } = useForm()
 
-
-  const userData =  (e) => {
-    setUser({
-      ...user, 
-      [e.target.name]:e.target.value
-    })
-  }
-
-  const finishOrder = (e)=> {
-    e.preventDefault()
-    if(!user.name || !user.surname || !user.email || !user.telephone || !user.address ) {
-      alert ('los campos son obligatorios')
-    } else if (user.email !== validate ) {
-      alert ('deben ser iguales')
-    } else {
-      let order = {
-        buyer: user,
-        cart: cart,
-        total: totalCart(),
-        date: serverTimestamp()
-      }
+  const onSubmit = (formData)=> {
+    let order = {
+      buyer: {
+        name: formData.name,
+        surname: formData.surname,
+        email: formData.email,
+        telephone: formData.telephone,
+        address: formData.address,
+      },
+      cart: cart,
+      total: totalCart(),
+      date: serverTimestamp()
+    }
       const sales = collection(db, 'orders')
       addDoc(sales, order)
       .then((res)=>{
@@ -49,78 +40,123 @@ function Checkout() {
         clear()
       })
       .catch((error)=> console.log(error))
-    }
+    
   }
 
   return (
     <>
       <div>
-        {orderId !== '' ? 
+        {orderId ? 
           <div>
             <SuccessfulOrder orderID={orderId}/>
           </div>
         : 
-          <Form className='checkout-form' onSubmit={finishOrder}>
+          <Form className='checkout-form' onSubmit={handleSubmit(onSubmit)}>
             <div>
               <h1 className='form-title text-uppercase'>Completa el formulario</h1>
               <p className='form-text'>Finaliza tu compra rellenando los siguientes campos:</p>
       
               <Form.Group controlId="formBasicName">
-                {/* <Form.Label>Email address</Form.Label> */}
-                <Form.Control type="text" name='name' placeholder="Nombre" onChange={userData} />
-                {/* <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text> */}
+                <Form.Control 
+                type="text" 
+                name='name' 
+                placeholder="Nombre" 
+                {...register("name", { required: true, minLength: 3 })}
+ 
+                />
+                {errors?.name?.type === "required" && (
+                  <p className='form-validate text-danger form-text text-start'>*Por favor complete el campo Nombre</p>
+                )}
+                {errors?.name?.type === "minLength" && (
+                  <p className='form-validate text-danger form-text text-start'>*El nombre debe contener al menos 3 caracteres</p>
+                )}
               </Form.Group>
       
               <Form.Group controlId="formBasicSurname">
-                {/* <Form.Label>Email address</Form.Label> */}
-                <Form.Control type="text" name='surname' placeholder="Apellidos" onChange={userData} />
-                {/* <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text> */}
+                <Form.Control 
+                type="text" 
+                name='surname' 
+                placeholder="Apellidos" 
+                {...register("surname", { required: true, minLength: 2 })}
+ 
+                />
+                {errors?.surname?.type === "required" && (
+                  <p className='form-validate text-danger form-text text-start'>*Por favor complete el campo Apellido</p>
+                )}
+                {errors?.surname?.type === "minLength" && (
+                  <p className='form-validate text-danger form-text text-start'>*El apellido debe contener al menos 2 caracteres</p>
+                )}
               </Form.Group>
       
               <Form.Group controlId="formBasicEmail">
-                {/* <Form.Label>Email address</Form.Label> */}
-                <Form.Control type="email" name='email' placeholder="Email" required onChange={userData} />
-                {/* <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text> */}
+                <Form.Control 
+                type="email" 
+                name='email' 
+                placeholder="E-mail" 
+                {...register("email", { required: true })}
+ 
+                />
+                {errors?.email?.type === "required" && (
+                  <p className='form-validate text-danger form-text text-start'>*Por favor complete el campo E-mail</p>
+                )}
               </Form.Group>
       
               <Form.Group controlId="formBasicRepeatEmail">
-                {/* <Form.Label>Email address</Form.Label> */}
                 <Form.Control 
                   type="email" 
-                  name='repeat-email' 
+                  name='repeatEmail' 
                   placeholder="Repetir email" 
-                  required 
-                  onChange={(e) => setValidate(e.target.value)} 
-                />
-                {/* <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text> */}
+                  {...register("repeatEmail", {required: true, validate:{equalEmails: repeatEmail => repeatEmail === getValues().email }})}
+   
+                  />
+                  {errors?.repeatEmail?.type === "required" && (
+                    <p className='form-validate text-danger form-text text-start'>*Por favor complete el campo Repetir E-mail</p>
+                  )}
+                  {errors?.repeatEmail?.type === "equalEmails" && (
+                    <p className='form-validate text-danger form-text text-start'>*Los e-mails deben ser iguales</p>
+                  )}
               </Form.Group>
       
               <Form.Group controlId="formBasicMobile">
-                {/* <Form.Label>Email address</Form.Label> */}
-                <Form.Control type="tel" name='telephone' placeholder="Teléfono" onChange={userData} />
-                {/* <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text> */}
+                <Form.Control 
+                type="tel" 
+                name='telephone' 
+                placeholder="Teléfono" 
+                {...register("telephone", { required: true, minLength: 9 })}
+ 
+                />
+                {errors?.telephone?.type === "required" && (
+                  <p className='form-validate text-danger form-text text-start'>*Por favor complete el campo Teléfono</p>
+                )}
+                {errors?.telephone?.type === "minLength" && (
+                  <p className='form-validate text-danger form-text text-start'>*El teléfono debe contener al menos 9 caracteres</p>
+                )}
               </Form.Group>
       
               <Form.Group controlId="formBasicAdress">
-                {/* <Form.Label>Password</Form.Label> */}
-                <Form.Control type="text" name='address' placeholder="Dirección" onChange={userData} />
+                <Form.Control 
+                type="text" 
+                name='address' 
+                placeholder="Dirección" 
+                 {...register("address", { required: true, minLength: 10, maxLength: 35})}
+ 
+                />
+                {errors?.address?.type === "required" && (
+                  <p className='form-validate text-danger form-text text-start'>*Por favor complete el campo Dirección</p>
+                )}
+                {errors?.address?.type === "minLength" && (
+                  <p className='form-validate text-danger form-text text-start'>*La dirección debe contener al menos 10 caracteres</p>
+                )}
+                {errors?.address?.type === "maxLength" && (
+                  <p className='form-validate text-danger form-text text-start'>*La dirección es demasiado extensa</p>
+                )}
               </Form.Group>
       
               {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Form.Check type="checkbox" label="Check me out" />
               </Form.Group> */}
       
-              <Button variant="dark" type="submit" className='w-100 btn-form text-uppercase'>
+              <Button variant="dark" type="submit" className='w-100 btn-form text-uppercase' disabled={!cart.length}>
                 Completar pedido
               </Button>
             </div>
